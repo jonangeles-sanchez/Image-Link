f8a3020e422b7d9e06bebb8b46f128a4b649c0c7
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-06-07 20:55:20 -0400

    wip

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,7 +1,21 @@
+import { useRef } from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import HorizontalScroll from "react-scroll-horizontal";
+
+import Info from "../../components/Info";
+
 function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Parallax pages={2}>
+        <ParallaxLayer offset={0} speed={1} factor={0.1}>
+          <Info />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={2} factor={0.1}>
+          <Info />
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
