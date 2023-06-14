30ef3b921614b1838162eca2fe7d38d904717941
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-06-14 19:41:05 -0400

    Create user route & controller

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,14 @@ connectDB();
 // Create an express app
 const app = express();
 
+// Add middleware to parse JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+// Add routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/imagelink", require("./routes/imageLinkRoutes"));
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
