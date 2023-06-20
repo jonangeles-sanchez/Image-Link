6450b55e00590c97262632e11f5b4232d6ce821d
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-06-20 17:22:47 -0400

    Implement image upload form - nonhttp

--- frontend/src/components/ImageLinkUpload.js
+++ frontend/src/components/ImageLinkUpload.js
@@ -7,21 +7,40 @@ const imagesUploadSchema = Yup.object().shape({
   picture: Yup.string().required("A file is required"),
 });
 const initialValues = {
-  picture: "",
+  picture: [],
 };
 
 function ImageLinkUpload() {
-  const handleSubmit = (e) => {
-    console.log("nothing");
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    //Upload images to the server
+    console.log(values);
+    const formData = new FormData();
+    console.log("Submitting form");
+    for (let i = 0; i < values.picture.length; i++) {
+      formData.append("picture", values.picture[i].name);
+    }
+    console.log(formData);
+    // Display the key/value pairs
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
+    }
+    // const response = await fetch("/api/upload", {
+    //   method: "POST",
+    //   body: formData,
+    // });
+    // const data = await response.json();
+    // console.log(data);
+    // //Reset the form
+    // resetForm();
   };
   return (
     <div className="upload-container">
       <div className="drop-container">
         <div className="drop-form">
           <Formik
-            onSubmit={handleSubmit}
+            onSubmit={handleFormSubmit}
             initialValues={initialValues}
-            validationSchema={imagesUploadSchema}
+            validator={() => ({})}
           >
             {({
               handleSubmit,
@@ -38,23 +57,45 @@ function ImageLinkUpload() {
                   acceptedFiles=".jpg,.jpeg,.png"
                   multiple={true}
                   onDrop={(acceptedFiles) => {
-                    setFieldValue("picture", acceptedFiles[0]);
+                    for (let i = 0; i < acceptedFiles.length; i++) {
+                      if (
+                        values.picture.find(
+                          (file) => file.name === acceptedFiles[i].name
+                        )
+                      ) {
+                        alert("File already exists in the list.");
+                        return;
+                      }
+                    }
+                    setFieldValue(
+                      "picture",
+                      values.picture.concat(acceptedFiles)
+                    );
                   }}
                 >
                   {({ getRootProps, getInputProps }) => (
                     <div {...getRootProps({ className: "dropzone" })}>
                       <input {...getInputProps()} />
-                      {!values.picture ? (
+                      {values.picture.length === 0 ? (
                         <p>
                           Drag 'n' drop some files here, or click to select
                           files
                         </p>
                       ) : (
-                        <p>Files selected: {values.picture.name}</p>
+                        //Print each image name in the array
+                        values.picture.map((file) => (
+                          <p key={file.name}>
+                            {file.name + " has been chosen."}
+                          </p>
+                        ))
                       )}
+                      {console.log(values)}
                     </div>
                   )}
                 </Dropzone>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
               </form>
             )}
           </Formik>
