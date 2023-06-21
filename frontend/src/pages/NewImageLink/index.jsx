548ce76844afc3969ca301255ae54b2e1110c36a
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-06-21 11:06:20 -0400

    Prevent user upload w/out account

--- frontend/src/pages/NewImageLink/index.jsx
+++ frontend/src/pages/NewImageLink/index.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Card from "../../components/Card";
 import ImageLinkUpload from "../../components/ImageLinkUpload";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 function NewImageLink() {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return (
     <Card>
       <ImageLinkUpload />
