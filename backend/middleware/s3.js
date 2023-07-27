bac215f5a719410cbcb6e3feb6b186bb67fd26f9
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-27 15:46:19 -0400

    Set up S3 Client w/ upload/get functions

---
+++ backend/middleware/s3.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+const fs = require("fs");
+// const S3 = require("aws-sdk/clients/s3");
+const { S3, S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { Upload } = require("@aws-sdk/lib-storage");
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+/*
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+*/
+const s3 = new S3Client({
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
+
+// uploads a file to s3
+function uploadFile(file, userId) {
+  const fileStream = fs.createReadStream(file.path);
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: userId + "_" + file.filename,
+  };
+
+  // return s3.upload(uploadParams).promise();
+  const upload = new Upload({
+    client: s3,
+    params: uploadParams,
+  });
+
+  return upload.done();
+}
+
+exports.uploadFile = uploadFile;
+
+// downloads a file from s3
+async function getFileStream(fileKey) {
+  const downloadParams = new GetObjectCommand({
+    Key: fileKey,
+    Bucket: bucketName,
+  });
+
+  const url = await getSignedUrl(s3, downloadParams);
+  return url;
+}
+
+exports.getFileStream = getFileStream;
