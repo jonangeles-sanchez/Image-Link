94f0456a5a577a97bb5c979a0091b36219752902
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-28 11:20:19 -0400

    Redo: frontpage

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,61 +1,86 @@
-import Card from "../../components/Card";
-import { motion } from "framer-motion";
-import Reveal from "../../components/Reveal";
-import moment_1 from "../../components/moment_1.mp4";
-import upload from "../../components/upload2.gif";
-import share from "../../components/assets/understand.png";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function index() {
+function Index() {
   return (
-    <div className="home-container">
-      <div className="home-small-text">
-        <Reveal delay={2}>
-          <h3>The best way to share your best moments across any device</h3>
-        </Reveal>
-      </div>
-      <div className="home-content">
-        <div className="home-capture">
-          <Reveal delay={3}>
-            <Card>
-              <video height={"50%"} width={"100%"} autoPlay loop muted>
-                {" "}
-                <source src={moment_1} type="video/mp4" />
-              </video>
-            </Card>
-          </Reveal>
-          <Reveal delay={3.5}>
-            <Card>
-              <p>
-                <span className="word-moment">Capture</span> your moments
-              </p>
-            </Card>
-          </Reveal>
+    <>
+      <div className="home-container">
+        <div className="logo">ImageLink</div>
+        <div className="welcome-text">
+          The best way to share your best moments across any device
+        </div>
+        <div className="home-buttons">
+          <Link to="/newimagelink" className="home-button">
+            New ImageLink
+          </Link>
+          <Link to="/login" className="home-button">
+            Login
+          </Link>
         </div>
 
-        <div className="home-upload">
-          <Reveal delay={3.5}>
-            <Card>
-              <p>
-                Easily <span className="word-moment">upload </span>
-                and create <span className="word-moment">ImageLinks</span>
-              </p>
-            </Card>
-          </Reveal>
-        </div>
+        <div className="features">
+          <div className="feature">
+            <div className="feature-icon">📸</div>
+            <div className="feature-title">Capture Moments</div>
+            <div className="feature-description">
+              Easily capture and store your precious moments with ImageLink.
+            </div>
+          </div>
 
-        <div className="home-upload">
-          <Reveal delay={3.5}>
-            <Card>
-              <p>
-                And <span className="word-moment-bl">share </span>
-                your <span className="word-moment-bl">ImageLink!</span>
-              </p>
-            </Card>
-          </Reveal>
+          <div className="feature">
+            <div className="feature-icon">💻</div>
+            <div className="feature-title">Cross-Device Access</div>
+            <div className="feature-description">
+              Access your images from any device, anytime, and anywhere.
+            </div>
+          </div>
+
+          <div className="feature">
+            <div className="feature-icon">🚀</div>
+            <div className="feature-title">Fast and Secure</div>
+            <div className="feature-description">
+              Experience fast and secure image hosting and sharing with
+              ImageLink.
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* How to use ImageLink Section */}
+      <section className="how-to-use-section">
+        <div className="how-to-use-card">
+          <div className="how-to-use-content">
+            <h2>How to use ImageLink:</h2>
+            <ol>
+              <li>
+                <strong>Sign Up:</strong> Create an account to start uploading
+                your images.
+              </li>
+              <li>
+                <strong>Create ImageLink:</strong> Click on "New ImageLink" to
+                create and customize your image collection.
+              </li>
+              <li>
+                <strong>Share:</strong> Once your ImageLink is ready, click on
+                "Share ImageLink" and easily share it with friends and family.
+              </li>
+            </ol>
+          </div>
+        </div>
+      </section>
+
+      <footer className="home-footer">
+        &copy; 2023 ImageLink. All rights reserved. | Made with ❤️ by{" "}
+        <a
+          href="https://www.yourwebsite.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Your Name
+        </a>
+      </footer>
+    </>
   );
 }
 
-export default index;
+export default Index;
