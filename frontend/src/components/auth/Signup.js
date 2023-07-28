395d3dde0185a33960e7b8db0fb23f40b61a801f
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-28 11:45:56 -0400

    Redo: Register page

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -61,63 +61,76 @@ function Register() {
 
   return (
     <>
-      <section className="heading">
-        <h1>Register</h1>
-        <p>Register to start sharing Images.</p>
-      </section>
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              value={name}
-              placeholder="Enter your name"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter your email"
-              name="email"
-              value={email}
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter your password"
-              name="password"
-              value={password}
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password2"
-              name="password2"
-              value={password2}
-              placeholder="Confirm password"
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-block">
-              signup
-            </button>
-          </div>
-        </form>
-      </section>
+      <div className="register-container">
+        <section className="heading">
+          <h1 className="register-header">Register</h1>
+          <p>Register to start sharing Images.</p>
+        </section>
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                placeholder="Enter your name"
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                name="email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                name="password"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password2">Confirm Password</label>
+              <input
+                type="password"
+                id="password2"
+                name="password2"
+                value={password2}
+                placeholder="Confirm password"
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-block">
+                Signup
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
+
+      <footer className="home-footer">
+        &copy; 2023 ImageLink. All rights reserved.
+        <a
+          href="https://www.yourwebsite.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jonhatan
+        </a>
+      </footer>
     </>
   );
 }
